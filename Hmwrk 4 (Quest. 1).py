1544c7d30712ac1f14d47fcde635f8f657a90ff9
Dominick S. George <111725500+Shaunblast@users.noreply.github.com>
2024-04-19 14:36:48 -0500

    Add files via upload

---
+++ Hmwrk 4 (Quest. 1).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 14:21:34 2024
+
+@author: domin
+"""
+
+
+
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+drift = 0.00 
+sigma = 0.01 
+I_Price = 280
+
+prices = np.zeros( ( 252, 1000 ) ) #Range is not inclusive on the end
+
+for i in range( 0, 1000 ): 
+    prices[ 0 ][ i ] = I_Price #Intial price is 280
+    
+for col in range( 0, 1000 ): # Calls each column (price path)
+    for row in range( 1, 252 ): #Calls each row after initial price of each column
+      
+        prices[ row ][ col ] = prices[ row - 1 ][ col ] * math.exp( drift * 1 - 0.5 * 
+                                                                   math.pow( sigma, 2 ) * 1 + sigma * np.random.normal( 0, 1, 1 ) ) 
+        
+plt.plot( prices ) 
+plt.show() 
